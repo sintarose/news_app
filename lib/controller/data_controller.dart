69b2390ee134b99f6ed2dae66ab8fa7e45260cc1
sintarose <sinta.rose@gmail.com>
2024-09-12 23:59:41 +0530

    changes

--- lib/controller/data_controller.dart
+++ lib/controller/data_controller.dart
@@ -2,8 +2,6 @@ import 'dart:io';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:news_app/constants/local_db.dart';
 import 'package:news_app/models/news_model.dart';
 
 class DataController extends ChangeNotifier {
@@ -18,7 +16,6 @@ class DataController extends ChangeNotifier {
   bool hasInternet = true;
   bool isLoading = true;
   final dio = Dio();
-  final dbHelper = DatabaseHelper();
 
   Future<void> _updateConnectionStatus(ConnectivityResult result) async {
     if (result == ConnectivityResult.mobile ||
@@ -27,7 +24,6 @@ class DataController extends ChangeNotifier {
       await fetchNewsFromAPI();
     } else {
       hasInternet = false;
-      await _loadNewsFromDB();
     }
     notifyListeners();
   }
@@ -40,11 +36,9 @@ class DataController extends ChangeNotifier {
         fetchNewsFromAPI();
       } else {
         hasInternet = false;
-         _loadNewsFromDB();
       }
     } on Exception catch (_) {
       hasInternet = false;
-      _loadNewsFromDB();
     }
   }
 
@@ -53,7 +47,6 @@ class DataController extends ChangeNotifier {
         'https://newsapi.org/v2/everything?q=bitcoin&from=2024-06-25&sortBy=publishedAt&apiKey=bd9934c750314feeb1fe65d556c9888c');
     if (response.statusCode == 200) {
       dataModel = NewsApiModel.fromJson(response.data);
-      saveNewsToDB();
       isLoading = false;
     } else {
       throw Exception('Failed to load news');
@@ -61,26 +54,7 @@ class DataController extends ChangeNotifier {
     notifyListeners();
   }
 
-  _loadNewsFromDB() async {
-    List<Article> articles = await dbHelper.getArticles();
-    dataModel = NewsApiModel(
-        status: 'ok', totalResults: articles.length, articles: articles);
-    isLoading = false;
-    notifyListeners();
-    isLoading = false;
 
-    if (dataModel?.articles == null) {
-      Get.snackbar('No valid network connection available', '',
-          snackPosition: SnackPosition.BOTTOM, backgroundColor: Colors.red);
-    } else {
-      Get.snackbar('No network connection', 'Loading local data');
-    }
-  }
 
-  saveNewsToDB() async {
-    await dbHelper.deleteAllArticles();
-    for (var article in dataModel!.articles!) {
-      await dbHelper.insertArticle(article);
-    }
-  }
+
 }
